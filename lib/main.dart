06f844366b58aa411bd930e5a2a5f6a233659e96
SakshiChaudhari123 <chaudhaisakshi-inft@atharvacoe.ac.in>
2023-05-03 11:08:08 +0530

    Committed

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,91 @@
 import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget { const MyApp({super.key});
 
-void main() {
-  runApp(const MyApp());
+@override
+Widget build(BuildContext context) {
+
+  return MaterialApp( debugShowCheckedModeBanner: false, home: MyForm(),
+  );
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(title: 'Flutter Container'),
-    );
-  }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyForm extends StatefulWidget {
+  const MyForm({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyForm> createState() => _MyFormState();
 }
+class _MyFormState extends State<MyForm> { final _formKey = GlobalKey<FormState>(); @override
+Widget build(BuildContext context) {
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  return Scaffold(
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+      appBar: AppBar(title: Text('Flutter Widgets' ,
+        style: TextStyle(fontSize: 30),) , centerTitle: true,),
 
-  @override
-  Widget build(BuildContext context) {
+      body: Container(
+        padding: EdgeInsets.all(30),
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            height: 200,
-            width: 750,
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.all(8),
-            color: Colors.blueGrey,
-            child: Text('Shubham Dhopat',textAlign: TextAlign.center,style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold)),
+        child: Form( key: _formKey, child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.person ,size: 50, color: Colors.blue,),
+              hintText: 'Enter your full name', labelText: 'Username',
+            ),
+            validator: (value) { if (value!.isEmpty) {
+              return 'Please Enter Some Text';
+            }
+            return null;
+            },
           ),
-          Container(
-            height: 200,
-            width: 750,
-            color: Colors.amber,
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.all(8),
 
-            child: Image.asset('assets/img/download.jpg'),
+          SizedBox( height: 20,
+
+
+
+
+
+
           ),
-          Container(
-            height: 200,
-            width: 750,
-            color: Colors.deepOrangeAccent,
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.all(8),
-            child: Text('TEIT-1    21',textAlign: TextAlign.center,style: TextStyle(fontSize: 32,fontStyle: FontStyle.italic)),
-          )
+
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.phone ,size: 50, color:Colors.blue,),
+
+              hintText: 'Enter a phone number', labelText: 'Phone',
+            ),
+            validator: (value) { if (value!.isEmpty) {
+              return 'Please Enter Valid Phone Number';
+            }
+            return null;
+            },
+          ),
+
+          SizedBox( height: 20,
+          ),
+
+          TextFormField(
+            keyboardType: TextInputType.datetime, decoration: const InputDecoration(
+            icon: const Icon(Icons.calendar_month ,size: 50, color:  Colors.blue,),
+            hintText: 'Enter Your Date Of Birth', labelText: 'Dob',
+          ),
+            validator: (value) { if (value!.isEmpty) {
+              return 'Please Enter Valid Date';
+            }
+            return null;
+            },
+          ),
+
+          new Container(
+              padding: const EdgeInsets.only(top: 40.0), child: new ElevatedButton(
+            child: Center(child: Text('Submit',style: TextStyle(fontSize: 25))),
+
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {}
+            },
+          )),
         ],
-      ),
-    );
-  }
-}
+        ),
+        ),));
+}}
